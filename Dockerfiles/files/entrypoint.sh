672060c427838c10aee591922b8e34b54c8bfe25
Acaila <matt@lonecloud.tech>
2026-03-17 20:45:55 +1100

    Changes required for local container runtime

--- Dockerfiles/files/entrypoint.sh
+++ Dockerfiles/files/entrypoint.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# First-run bootstrap of /etc/raas/raas.conf if missing
-if [ ! -s /etc/raas/raas.conf ]; then
-  echo "[entrypoint] Seeding /etc/raas/raas.conf from /bootstrap/raas.conf"
-  cp /bootstrap/raas.conf /etc/raas/raas.conf
+mkdir -p /etc/raas /var/log/raas
+
+if [ ! -s /etc/raas/raas ]; then
+  echo "[entrypoint] Seeding /etc/raas/raas from /bootstrap/raas"
+  cp /bootstrap/raas /etc/raas/raas
 fi
 
-# Optional environment substitutions for convenience
-# RAAS_DB_HOST/NAME/USER/PASS, RAAS_REDIS_HOST/PORT
-sed -i "s#@@DB_HOST@@#${RAAS_DB_HOST:-postgres}#g" /etc/raas/raas.conf
-sed -i "s#@@DB_NAME@@#${RAAS_DB_NAME:-raas}#g" /etc/raas/raas.conf
-sed -i "s#@@DB_USER@@#${RAAS_DB_USER:-raas}#g" /etc/raas/raas.conf
-sed -i "s#@@DB_PASS@@#${RAAS_DB_PASS:-changeme}#g" /etc/raas/raas.conf
-sed -i "s#@@REDIS_HOST@@#${RAAS_REDIS_HOST:-redis}#g" /etc/raas/raas.conf
-sed -i "s#@@REDIS_PORT@@#${RAAS_REDIS_PORT:-6379}#g" /etc/raas/raas.conf
+sed -i "s#@@DB_HOST@@#${RAAS_DB_HOST:-postgres}#g" /etc/raas/raas
+sed -i "s#@@REDIS_HOST@@#${RAAS_REDIS_HOST:-redis}#g" /etc/raas/raas
+sed -i "s#@@REDIS_PORT@@#${RAAS_REDIS_PORT:-6379}#g" /etc/raas/raas
 
-# Permissions hygiene
-chown -R ssc:root /etc/raas /var/log/raas
+if [ ! -f /etc/raas/raas.secconf ]; then
+  echo "[entrypoint] Creating /etc/raas/raas.secconf with raas save_creds"
+  /opt/saltstack/raas/bin/raas -c /etc/raas save_creds \
+    "postgres={\"username\":\"${RAAS_DB_USER:-raas}\",\"password\":\"${RAAS_DB_PASS:-raaspass}\"}" \
+    "redis={\"password\":\"${RAAS_REDIS_PASS:-}\"}"
+fi
+
+chmod 600 /etc/raas/raas.secconf || true
 
 exec "$@"
